e2b091a9ac619f8e015f4a5c1911b36e58345892
mrberkay <berqqay@hotmail.com>
2020-09-18 21:14:26 +0200

    function definition

--- CarryOne/CarryOne.cpp
+++ CarryOne/CarryOne.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <math.h>
 using namespace std;
 
-int main()
+vector<int> carryOne(vector<int> v)
 {
-	vector<int> v = {0, 5, 0, 8, 3, 2};
 	int number;
 	int result = 0;
 	int var;
-	int size = v.size();
+	int vectorSize = v.size();
+	int base = pow(10, vectorSize - 1);
+	// Convert vector content to integer
+	for(auto i = v.begin(); i != v.end(); i++)
+	{
+		var = *i;
+		number = var * base;
+		result += number;	
+		base = base / 10;		
+	}
+	cout << "number in array: " <<  result << endl;
+	// Increment by one
+	result += 1;
+	
+}
+
+void print(vector<int> v)
+{
+	for(auto i = v.begin(); i != v.end(); i++) cout << *i;
+}
+
+
+int main()
+{
+	vector<int> v1 = {5, 0, 8, 3, 2};
+	print(v1);
+
 
 }
 
