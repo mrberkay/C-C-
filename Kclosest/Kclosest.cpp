c997b3a91e0046fc9e84020d553a5dbbe9b90368
mrberkay <berqqay@hotmail.com>
2020-09-21 22:44:07 +0200

    new algorithm

---
+++ Kclosest/Kclosest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	int points[12] = {-2,4,0,-2,-1,0,3,5,-2,-3,3,2};
+	int k = 1;
+	int arraySize = *(&points + 1) - points;
+
+}
+
+
+
+
+
+
